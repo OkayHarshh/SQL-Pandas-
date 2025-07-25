930f276578db58fdaaecc7d5374a475697f54bce
OkayHarshh <harshkumarm2005@gmail.com>
2025-07-26 00:42:36 +0530

    sql

---
+++ marvel.sql
@@ -0,0 +1,9 @@
+--  List all Marvel movies released after 2015.
+
+SELECT * FROM marvel
+WHERE year > 2015
+
+-- Show DC movies with a box office collection greater than 500 crores.
+
+SELECT * FROM dc
+WHERE collection > 500
